e2bd9e0748d773a2b88e7e51bc6960351914ec6c
Nicolás Gómez <skarddy@gmail.com>
2024-12-20 13:05:27 -0500

    New Dialogues

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -6,9 +6,20 @@ using DialogueEditor;
 
 public class DialogueManager : MonoBehaviour
 {
-    public NPCConversation firstConversation;
-
+    public NPCConversation[] Conversations;
     public void Start(){
-        ConversationManager.Instance.StartConversation(firstConversation);
+        ConversationManager.Instance.StartConversation(Conversations[0]);
+    }
+    public void OnLoggedIn(){
+        ConversationManager.Instance.StartConversation(Conversations[1]);
+    }
+
+    public void OnInternetConnection(){
+        StartCoroutine(InternetConnection());
+    }
+
+    IEnumerator InternetConnection(){
+        yield return new WaitForSeconds(1.5f);
+        ConversationManager.Instance.StartConversation(Conversations[2]);
     }
 }
