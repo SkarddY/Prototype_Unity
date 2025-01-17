0398094222f4c519b6e664539ad95b2bf1f30fc9
Nicolás Gómez <skarddy@gmail.com>
2025-01-17 00:10:37 -0500

    Dialogues Finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,34 @@
 using System;
-using Unity.VisualScripting;
+using System.Collections;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private UnityEvent onLogIn;
+    [SerializeField] private UnityEvent onLogIn, onLoseScreen;
     
     public static GameManager Instance {get; private set;}
 
    public void LogIn(){
         onLogIn.Invoke();
+    }
+
+    public void LoseScreen(){
+        onLoseScreen.Invoke();
+        StartCoroutine(DoRestart());
+    }
+
+    IEnumerator DoRestart(){
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnStartGame(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnMenuReturn(){
+        SceneManager.LoadScene(0);
     }
 }
