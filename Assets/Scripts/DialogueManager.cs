1d8a902859a92a82b5838f71799af17b6b9b759c
Nicolás Gómez <skarddy@gmail.com>
2025-03-04 21:40:18 -0500

    Conected First Minigame to main project

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,21 +1,20 @@
 using DialogueEditor;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-//using DialogueEditor;
-
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField] NPCConversation[] Conversations;
-    /*public void Awake(){
+    public void Awake() {
         ConversationManager.Instance.StartConversation(Conversations[0]);
-    }*/
+    }
 
-    //BUTTONS FOR CONVERSATION CHANGES
-    /*public void OnStartGame() {
-        ConversationManager.Instance.StartConversation(Conversations[0]);      
-    } */
-    public void OnFinishedIntro(){
+    public void OnTutorialEnd() {
+        Conversations[0].gameObject.SetActive(false);
         ConversationManager.Instance.StartConversation(Conversations[1]);
     }
-}
+
+    public void OnFirstMinigameEnd() { 
+        ConversationManager.Instance.EndConversation();
+        ConversationManager.Instance.StartConversation(Conversations[2]);
+    }
+} 
+
