0be33f629bdf59a82c0db5baaf51dd5be4e52d85
Nicolás Gómez <skarddy@gmail.com>
2025-03-12 18:24:33 -0500

    Final timer and menu fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,32 +1,13 @@
-using System;
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private UnityEvent onLogIn, onLoseScreen;
     [SerializeField] private GameObject errorPanel;
     
-    public static GameManager Instance {get; private set;}
-
-   public void LogIn(){
-        onLogIn.Invoke();
-    }
-
-    public void LoseScreen(){
-        onLoseScreen.Invoke();
-        StartCoroutine(DoRestart());
-    }
-
     public void ErrorButton(){
         StartCoroutine(DoError());
-    }
-
-    IEnumerator DoRestart(){
-        yield return new WaitForSeconds(5.0f);
-        SceneManager.LoadScene(0);
     }
 
     IEnumerator DoError(){
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,17 +2,32 @@ using TMPro;
 using UnityEngine;
 public class Timer : MonoBehaviour
 {
-    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI timerText, finalTimeText;
     private float elapsedTime;
+    private bool gameEnded = false;
 
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        UpdateTimerText();
+        if(!gameEnded) {
+            elapsedTime += Time.deltaTime;
+            UpdateTimerText();
+        }
     }
     void UpdateTimerText() {
         int minutesElapsed = Mathf.FloorToInt(elapsedTime / 60);
         int secondsElapsed = Mathf.FloorToInt(elapsedTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutesElapsed, secondsElapsed);
+    }
+
+    public void EndGame() {
+        gameEnded = true;
+        PlayerPrefs.SetFloat("FinalTime", elapsedTime);
+        DisplayFinalTime();
+    }
+    void DisplayFinalTime() {
+        float finalTime = PlayerPrefs.GetFloat("FinalTime");
+        int minutesElapsed = Mathf.FloorToInt(finalTime / 60);
+        int secondsElapsed = Mathf.FloorToInt(finalTime % 60);
+        finalTimeText.text = string.Format("TIEMPO: {0:00}:{1:00}", minutesElapsed, secondsElapsed);
     }
 }
